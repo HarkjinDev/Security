a1ee940d7e56c3a33fe36cc56f8ab3b36923cb1c
Harkjin.dev <harkjin.dev@gmail.com>
2021-06-02 11:15:48 +0900

    Update binarysearch.py

--- Algorithm/BinarySearch/binarysearch.py
+++ Algorithm/BinarySearch/binarysearch.py
@@ -2,6 +2,9 @@ import sys
 
 def binary_search(arr, target, left=None, right=None):
 
+    # the arr data must be sorted
+    arr.sort()
+
     left, right = 0, len(arr) - 1
 
     while left <= right:
@@ -11,7 +14,7 @@ def binary_search(arr, target, left=None, right=None):
         if arr[mid] > target:
             right = mid -1
         if arr[mid] == target:
-            return mid
+            return mid + 1
         if arr[mid] < target:
             left = mid + 1
             
@@ -29,13 +32,16 @@ def binary_search_recursion(arr, target, left=None, right=None):
     if arr[mid] > target:
         return binary_search_recursion(arr, target, left, mid-1)
     if arr[mid] == target:
-        return mid
+        return mid + 1
     if arr[mid] < target:
         return binary_search_recursion(arr, target, mid+1, right)
 
-
+# Set maximum recursion depth
 sys.setrecursionlimit(10000)
 
-# This is test part
+##### This is test part #####
+
 print("binary_search : ", binary_search(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "N", "O", "P"], "G"))
+
+# the arr data must be sorted
 print("binary_search_recursion : ", binary_search_recursion(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "N", "O", "P"], "G"))
