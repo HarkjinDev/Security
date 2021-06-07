f029b33ad6f52f9a23c780ae5d801ab3e177c17e
Harkjin.dev <harkjin.dev@gmail.com>
2021-06-07 17:08:26 +0900

    Update conversion.c

--- Reverse/HackMe/Level7/conversion.c
+++ Reverse/HackMe/Level7/conversion.c
@@ -1,4 +1,4 @@
-// This is conversion of binary sign to binary code and decimal and ascii code
+// This is conversion of the singal code to binary and decimal and ascii code
 // and get the level8's password
 
 #include <stdio.h>
