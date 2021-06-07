dec14ae140d541e51b203e49d0132c2cc1881b13
Harkjin.dev <harkjin.dev@gmail.com>
2021-06-07 15:02:07 +0900

    Update tn.c

--- Reverse/HackMe/Level6/tn.c
+++ Reverse/HackMe/Level6/tn.c
@@ -1,5 +1,6 @@
 // This is the restore code of tn.s
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <signal.h>
 
@@ -8,15 +9,15 @@ void sig_func(int signo)
         printf("Can't use ctrl+c\n");
 }
 
-int main()
+
+main()
 {
-        char input;
-        int select, i;
+
+        int input;
 
         system("cat hint");
-        input = getchar();
+        getchar();
         system("clear");
-
         printf("#####################################\n");
         printf("##                                 ##\n");
         printf("##        텔넷 접속 서비스         ##\n");
@@ -25,22 +26,19 @@ int main()
         printf("##     3. 천리안                   ##\n");
         printf("##                                 ##\n");
         printf("#####################################\n");
-
-        for(i=1; i<32; i++)
-        {
-                if(i == SIGINT) signal(i, sig_func);
-                else signal(i, SIG_IGN);
-        }
+        signal(0x2, sig_func);
 
         printf("\n접속하고 싶은 bbs를 선택하세요 : ");
-        switch(input)
-        {
-                case 1: system("telnet 203.245.15.76"); break;
-                case 2: system("telnet 203.238.129.97"); break;
-                case 3: system("telnet 210.120.128.180"); break;
-                default:
-                        if(input !=1 && input !=2 && input !=3)
-                                printf("잘못 입력하셨습니다. 접속을 종료합니다.\n");
-        }
+        scanf("%d", input);
+
+        if (input == 1)
+                system("telnet 203.245.15.76");
+        if (input == 2)
+                system("telnet 203.238.129.97");
+        if (input == 3)
+                system("telnet 210.120.128.180");
+        if (input != 1 && input !=2 && !=3)
+                printf("잘못 입력하셨습니다. 접속을 종료합니다.\n");
+        
         return 0;
 }
