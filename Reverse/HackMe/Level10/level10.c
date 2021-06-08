b36e55824d0e5a59ee32d7aca9a234c28027f41f
Harkjin.dev <harkjin.dev@gmail.com>
2021-06-08 16:48:59 +0900

    Create level10.c

---
+++ Reverse/HackMe/Level10/level10.c
@@ -0,0 +1,21 @@
+// This is the restore code of level10.s
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+char *text;
+int shmid;
+
+int main( void ) {
+
+        shmid = shmget((key_t)7530, 1028, 1666 | IPC_CREAT);
+        text = shmat(shmid, (void *)0, 0);
+
+        strcpy(text,"멍멍: level11의 패스워드는?\n구타: what!@#$?\n");
+
+}
