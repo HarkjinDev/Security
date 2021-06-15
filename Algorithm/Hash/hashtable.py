f3355e94362d7fe0bbfef5091d561bc686f940f9
Harkjin.dev <harkjin.dev@gmail.com>
2021-06-15 15:12:43 +0900

    Create hashtable.py

---
+++ Algorithm/Hash/hashtable.py
@@ -0,0 +1,49 @@
+# Hash Table
+class HashTable:
+    def __init__(self, table_size):
+        self.size = table_size
+        self.hash_table = [0 for a in range(self.size)]
+        
+    def getKey(self, data):
+        # ord = return ACSII
+        self.key = ord(data[0])
+        return self.key
+    
+    def hashFunction(self, key):
+        # Division method (hash table size(m) is usually prime number)
+        return key % self.size
+
+    def getAddress(self, key):
+        myKey = self.getKey(key)
+        hash_address = self.hashFunction(myKey)
+        return hash_address
+    
+    def save(self, key, value):
+        hash_address = self.getAddress(key)
+        self.hash_table[hash_address] = value
+        
+    def read(self, key):
+        hash_address = self.getAddress(key)
+        return self.hash_table[hash_address]
+    
+    def delete(self, key):
+        hash_address = self.getAddress(key)
+        
+        if self.hash_table[hash_address] != 0:
+            self.hash_table[hash_address] = 0
+            return True
+        else:
+            return False
+
+#Test Code
+h_table = HashTable(8)
+h_table.save('a', '1111')
+h_table.save('b', '3333')
+h_table.save('c', '5555')
+h_table.save('d', '8888')
+print(h_table.hash_table)
+print(h_table.read('d'))
+
+h_table.delete('d')
+
+print(h_table.hash_table)
