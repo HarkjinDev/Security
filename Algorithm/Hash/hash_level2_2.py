4cc054957b609e88d30c2aaee6f6a7995a966da8
Harkjin.dev <harkjin.dev@gmail.com>
2021-06-15 15:08:52 +0900

    Create hash_level2_2.py

---
+++ Algorithm/Hash/hash_level2_2.py
@@ -0,0 +1,22 @@
+# Programmers - Hash level2_2
+# https://programmers.co.kr/learn/courses/30/lessons/42578
+
+from collections import *
+
+def solution(clothes):
+
+    dic = defaultdict(list)
+    answer = 1
+
+    for item, category in clothes:
+        dic[category].append(item)
+
+    for i in dic.keys():
+        answer *= len(dic[i]) + 1
+
+    answer -= 1
+
+    return answer
+
+# Test Part
+print(solution([["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]))
