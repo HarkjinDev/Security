27654873bb3d7a5442c12206ad35d791ed3303d9
Harkjin.dev <harkjin.dev@gmail.com>
2021-07-08 00:58:51 +0900

    Update lambda_function.py

--- AWS_Serverless_RESTAPI/lambda_function.py
+++ AWS_Serverless_RESTAPI/lambda_function.py
@@ -4,7 +4,19 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 
 def lambda_handler(event, context):
-  return {
+    
+    table = dynamodb.Table('Fishing_Notice')
+    
+    TableCounter = table.scan(Select="COUNT").get("Count")
+    
+    body = table.get_item(
+        Key={
+            "no": TableCounter
+        }
+    )
+    response = body['Item']
+    
+    return {
         'statusCode': 200,
-        'body': "Hello"
-  }
+        'body': response.get("message")
+    }
