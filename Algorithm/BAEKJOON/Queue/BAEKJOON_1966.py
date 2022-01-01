a3441e0132e12c8cb031df87ab4b90c13a9276cd
MOON HAKJIN <harkjin.dev@gmail.com>
2022-01-01 14:36:28 +0900

    Create BAEKJOON_1966.py

---
+++ Algorithm/BAEKJOON/Queue/BAEKJOON_1966.py
@@ -0,0 +1,34 @@
+# 프린터 큐
+# https://www.acmicpc.net/problem/1966
+from collections import deque
+
+T = int(input())
+
+for _ in range(T):
+  N, M = map(int, input().split())
+  imp = list(map(int, input().split()))
+  
+  q = deque()
+  for i in imp:
+    q.append(i)
+  
+  idx = deque()
+  for i in range(0, N):
+    idx.append(i)
+
+  cnt = 0
+
+  while q:
+      if q[0] == max(q):
+        cnt += 1   
+        if idx[0] == M:
+          print(cnt)
+          break
+        else:
+          q.popleft()
+          idx.popleft()
+      else:
+        q.append(q.popleft())
+        idx.append(idx.popleft())
+      
+      
